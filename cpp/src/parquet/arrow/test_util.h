f69ab13516e5b7bc3e32b3a4b20a25bdb01c0aa0
mwish <maplewish117@gmail.com>
2023-11-04 14:40:46 +0800

    update: split out page-index-test

--- cpp/src/parquet/arrow/test_util.h
+++ cpp/src/parquet/arrow/test_util.h
@@ -401,9 +401,10 @@ template <class ArrowType>
 /// Wrap an Array into a ListArray by splitting it up into size lists.
 ///
 /// This helper function only supports (size/2) nulls.
-Status MakeListArray(const std::shared_ptr<Array>& values, int64_t size,
-                     int64_t null_count, const std::string& item_name,
-                     bool nullable_values, std::shared_ptr<::arrow::ListArray>* out) {
+inline Status MakeListArray(const std::shared_ptr<Array>& values, int64_t size,
+                            int64_t null_count, const std::string& item_name,
+                            bool nullable_values,
+                            std::shared_ptr<::arrow::ListArray>* out) {
   // We always include an empty list
   int64_t non_null_entries = size - null_count - 1;
   int64_t length_per_entry = values->length() / non_null_entries;
@@ -439,7 +440,7 @@ Status MakeListArray(const std::shared_ptr<Array>& values, int64_t size,
 }
 
 // Make an array containing only empty lists, with a null values array
-Status MakeEmptyListsArray(int64_t size, std::shared_ptr<Array>* out_array) {
+inline Status MakeEmptyListsArray(int64_t size, std::shared_ptr<Array>* out_array) {
   // Allocate an offsets buffer containing only zeroes
   const int64_t offsets_nbytes = (size + 1) * sizeof(int32_t);
   ARROW_ASSIGN_OR_RAISE(auto offsets_buffer, ::arrow::AllocateBuffer(offsets_nbytes));
@@ -463,14 +464,14 @@ Status MakeEmptyListsArray(int64_t size, std::shared_ptr<Array>* out_array) {
   return Status::OK();
 }
 
-std::shared_ptr<::arrow::Table> MakeSimpleTable(
+inline std::shared_ptr<::arrow::Table> MakeSimpleTable(
     const std::shared_ptr<ChunkedArray>& values, bool nullable) {
   auto schema = ::arrow::schema({::arrow::field("col", values->type(), nullable)});
   return ::arrow::Table::Make(schema, {values});
 }
 
-std::shared_ptr<::arrow::Table> MakeSimpleTable(const std::shared_ptr<Array>& values,
-                                                bool nullable) {
+inline std::shared_ptr<::arrow::Table> MakeSimpleTable(
+    const std::shared_ptr<Array>& values, bool nullable) {
   auto carr = std::make_shared<::arrow::ChunkedArray>(values);
   return MakeSimpleTable(carr, nullable);
 }
@@ -494,7 +495,7 @@ void ExpectArrayT(void* expected, Array* result) {
 }
 
 template <>
-void ExpectArrayT<::arrow::BooleanType>(void* expected, Array* result) {
+inline void ExpectArrayT<::arrow::BooleanType>(void* expected, Array* result) {
   ::arrow::BooleanBuilder builder;
   ARROW_EXPECT_OK(
       builder.AppendValues(reinterpret_cast<uint8_t*>(expected), result->length()));
